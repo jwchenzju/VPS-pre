21132aa0a43dccc2a5b6454a2d4f722bf89d142b
菜鸟之之 <30511057+jwchenzju@users.noreply.github.com>
2022-05-06 08:12:09 +0800

    Update 1keydocker.sh

--- 1keydocker.sh
+++ 1keydocker.sh
@@ -111,6 +111,7 @@ installssr(){
     podman run -d --net host --name ssr --restart=always -v /etc/shadowsocks-r:/etc/shadowsocks-r teddysun/shadowsocks-r
     podman generate systemd --restart-policy always -t 1 -n -f ssr
     mv container-ssr.service /etc/systemd/system/
+    chmod +x /etc/rc.d/rc.local
     restorecon -RvF /etc/systemd/system/container-ssr.service
     systemctl daemon-reload
     systemctl enable container-ssr.service --now
