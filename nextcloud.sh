749d4f2870530aa196defe945a4eebdf5e0bd6e6
菜鸟之之 <30511057+jwchenzju@users.noreply.github.com>
2022-09-02 19:47:21 +0800

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -1,16 +1,8 @@
 #1KEYDOCKER安装后，可安装网盘
-firewall-cmd --permanent --add-port=8888/tcp
-podman pull docker.io/nextcloud
-podman create -p 8888:80 --log-driver k8s-file \
--v nextcloud:/var/www/html \
---log-opt path=/var/log/nextcloud.log \
---log-opt max-size=50m \
---name nextcloud \
-nextcloud
-
-#添加为自动运行
-podman generate systemd --restart-policy always -t 1 -n -f nextcloud
-mv container-nextcloud.service /etc/systemd/system/
-restorecon -R /etc/systemd/system/container-nextcloud.service
-systemctl daemon-reload
-systemctl enable container-nextcloud.service
+docker run -d -p 8888:80 \
+    -v nextcloud:/var/www/html \
+    --log-driver local \
+    --log-opt max-size=10m \
+    --log-opt max-file=5 \
+    --name nextcloud \
+    nextcloud
