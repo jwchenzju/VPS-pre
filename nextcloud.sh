03d80ee39b629c9fdd7114f556f4163b73168ec7
菜鸟之之 <30511057+jwchenzju@users.noreply.github.com>
2022-09-21 20:14:45 +0800

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -2,7 +2,5 @@
 docker run --restart=always -d -p 8888:80 \
     -v nextcloud:/var/www/html \
     --log-driver local \
-    --log-opt max-size=10m \
-    --log-opt max-file=5 \
     --name nextcloud \
     nextcloud
