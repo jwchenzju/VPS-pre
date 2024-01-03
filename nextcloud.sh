3fee4c13f67b1d7eab8e19374621579bc8272e53
菜鸟之之 <30511057+jwchenzju@users.noreply.github.com>
2024-01-03 20:34:35 +0800

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -1,6 +1,6 @@
 #1KEYDOCKER安装后，可安装网盘
 docker run --restart=always -d -p 8888:80 \
-    -v nextcloud:/var/www/html \
+    -v /nextcloud:/var/www/html \
     --log-driver local \
     --name nextcloud \
     nextcloud
