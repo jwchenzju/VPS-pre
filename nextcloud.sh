cc9f4b0541a0dce23b827b8db726f5e9527aa31e
菜鸟之之 <30511057+jwchenzju@users.noreply.github.com>
2025-02-19 17:30:37 +0800

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -3,7 +3,5 @@
 docker run --restart always -d -p 8888:80 \
     -v /nextcloud:/var/www/html \
     --log-driver local \
-    --env PHP_MEMORY_LIMIT=2048M \
-    --env PHP_UPLOAD_LIMIT=2048M \
     --name nextcloud \
     nextcloud:30.0.5
